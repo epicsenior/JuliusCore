42792e705aeb14a5ec7d0072fe30817885daaebd
liujulius@outlook.com <liujulius@outlook.com>
2014-12-10 16:32:49 -0800

    - first commit - Classic Nancy/Owin Empty ASP.Net Setup

---
+++ Scratch/Index.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Scratch
+{
+
+    public class Index : NancyModule
+    {
+
+        public Index()
+        {
+            Get["/"] = x => {
+
+                return View["Index.html"];
+            };
+        }
+    }
+}
---
+++ Scratch/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Scratch.Startup))]
+namespace Scratch
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
